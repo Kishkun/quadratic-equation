3c8efec056daf2b3be0d0c4ed8ea57fee999d58f
Kishkun <durata55@gmail.com>
2018-09-24 19:34:50 +0300

    add result equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let newArr = [];
+    let arr = equation.split(' ');
+    let a = (arr[0]);
+    let b = (arr[3] + arr[4]);
+    let c = (arr[7] + arr[8]);
+    let resultArr = (b * b - 4 * a * c);
+
+    if (resultArr > 0) {
+        let d = Math.round(Math.sqrt(resultArr));
+        newArr[0] = ((-b) + d) / (2 * a);
+        newArr[1] = ((-b) - d) / (2 * a);
+    } else {
+        return null;
+    }
+    Math.round(newArr);
+    newArr.sort(compareNumeric);
+    return newArr;
+};
+
+function compareNumeric(x, y) {
+    return x - y;
 }
